5bf38bc894a0f6f4bfd52f0fbf1e4200996f7feb
Rafał Florczak <florczak.raf@gmail.com>
2025-12-08 22:05:29 +0100

    08: opt

--- 08/solve_a.py
+++ 08/solve_a.py
@@ -2,32 +2,35 @@
 import sys
 
 edges = {}
-nodes = sys.stdin.read().splitlines()
+nodes = [tuple(int(x) for x in l.split(",")) for l in sys.stdin.read().splitlines()]
 n_ops = 10 if len(nodes) == 20 else 1000
+costs = set()
 for i in range(len(nodes)):
     for j in range(i + 1, len(nodes)):
-        x,y,z = (int(n) for n in nodes[i].split(","))
-        xx,yy,zz = (int(n) for n in nodes[j].split(","))
+        x,y,z = nodes[i]
+        xx,yy,zz = nodes[j]
         cost = (x - xx)**2 + (y - yy)**2 + (z - zz)**2
-        edges[(nodes[i], nodes[j])] = cost
+        edges[cost] = (nodes[i], nodes[j])
+        costs.add(cost)
 
+sorted_costs = sorted(costs)
 connected = []
-sorted_edges = sorted(edges.items(), key=lambda x: x[1])
 for op in range(n_ops):
-    (n1, n2), _ = sorted_edges[op]
-    to_combine = []
+    n1, n2 = edges[sorted_costs[op]]
+    previously_updated = None
     for i, g in enumerate(connected):
         if n1 in g or n2 in g:
-            g.add(n1)
-            g.add(n2)
-            to_combine.append(i)
-    if not to_combine:
-        connected.append({n1, n2})
-
-    if len(to_combine) >= 2:
-        sorted_to_combine = sorted(to_combine)
-        for i in sorted_to_combine[1:]:
-            connected[sorted_to_combine[0]].update(connected.pop(i))
+            if not previously_updated:
+                g.add(n1)
+                g.add(n2)
+                previously_updated = i
+            else:
+                g.update(connected[previously_updated])
+                connected.pop(previously_updated)
+                break
+    else:
+        if not previously_updated:
+            connected.append({n1, n2})
 
 sc = sorted(connected, key=lambda x: len(x), reverse=True)
 print(len(sc[0]) * len(sc[1]) * len(sc[2]))
--- 08/solve_b.py
+++ 08/solve_b.py
@@ -2,42 +2,28 @@
 import sys
 
 edges = {}
-nodes = sys.stdin.read().splitlines()
+nodes = [tuple(int(x) for x in l.split(",")) for l in sys.stdin.read().splitlines()]
+n_ops = 10 if len(nodes) == 20 else 1000
+costs = set()
 for i in range(len(nodes)):
     for j in range(i + 1, len(nodes)):
-        x,y,z = (int(n) for n in nodes[i].split(","))
-        xx,yy,zz = (int(n) for n in nodes[j].split(","))
+        x,y,z = nodes[i]
+        xx,yy,zz = nodes[j]
         cost = (x - xx)**2 + (y - yy)**2 + (z - zz)**2
-        edges[(nodes[i], nodes[j])] = cost
+        edges[cost] = (nodes[i], nodes[j])
+        costs.add(cost)
 
-connected = []
+sorted_costs = sorted(costs)
 unconnected = set(nodes)
-sorted_edges = sorted(edges.items(), key=lambda x: x[1])
 op = 0
 while len(unconnected) > 1:
-    (n1, n2), _ = sorted_edges[op]
-    to_combine = []
-    for i, g in enumerate(connected):
-        if n1 in g or n2 in g:
-            g.add(n1)
-            g.add(n2)
-            to_combine.append(i)
-    if not to_combine:
-        connected.append({n1, n2})
-
-    if len(to_combine) >= 2:
-        sorted_to_combine = sorted(to_combine)
-        for i in sorted_to_combine[1:]:
-            connected[sorted_to_combine[0]].update(connected.pop(i))
-
+    n1, n2 = edges[sorted_costs[op]]
     op += 1
     unconnected.discard(n1)
     unconnected.discard(n2)
 
 last = unconnected.pop()
-print(last)
-for ns, _ in sorted_edges[op:]:
-    if last in ns:
-        print(ns)
-        print(int(ns[0].split(",")[0]) * int(ns[1].split(",")[0]))
+for cost in sorted_costs[op:]:
+    if last in edges[cost]:
+        print(int(edges[cost][0][0]) * int(edges[cost][1][0]))
         break
