4301032037c3fefca83e1db600a2d0ea70d6c03a
Rafał Florczak <florczak.raf@gmail.com>
2025-12-11 17:19:26 +0100

    11b

--- 11/solve_b.py
+++ 11/solve_b.py
@@ -8,7 +8,7 @@ for l in lines:
     source, dests = l.split(": ")
     nodes[source] = dests.split()
 
-def dfs_counter(s, t, seen):
+def dfs(s, t, seen):
     if s == t:
         return 1
 
@@ -17,42 +17,14 @@ def dfs_counter(s, t, seen):
 
     paths = 0
     for next in nodes.get(s, []):
-        paths += dfs_counter(next, t, seen)
+        paths += dfs(next, t, seen)
 
     seen[s] = paths
     return paths
 
-def dfs_path(s, t, seen):
-    if s == t:
-        return [[t]]
+fft_to_dac = dfs("fft", "dac", {})
 
-    if s in seen:
-        return seen[s]
-
-    paths = []
-    for next in nodes.get(s, []):
-        subpaths = dfs_path(next, t, seen)
-        for p in subpaths:
-            paths.append([s] + p)
-
-    seen[s] = paths
-    return paths
-
-print("svr->fft", dfs_counter("svr", "fft", {}))
-print("fft->out", dfs_counter("fft", "out", {}))
-print()
-print("svr->dac", dfs_counter("svr", "dac", {}))
-print("dac->out", dfs_counter("dac", "out", {}))
-print()
-print("dac->fft", dfs_counter("dac", "fft", {}))
-print("fft->dac", dfs_counter("fft", "dac", {}))
-paths_to_fft = dfs_path("svr", "fft", {})
-paths_from_fft_to_dac = dfs_path("fft", "dac", {})
-n_paths_from_fft = dfs_counter("fft", "out", {})
-total = 0
-print(len(paths_to_fft))
-for p in paths_to_fft:
-    if "dac" in p:
-        total += 1
-total *= n_paths_from_fft
-print(total)
+if fft_to_dac:
+    print(dfs("svr", "fft", {}) * fft_to_dac * dfs("dac", "out", {}))
+else:
+    print(dfs("svr", "dac", {}) * dfs("dac", "fft", {}) * dfs("fft", "out", {}))
